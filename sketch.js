15798fee954eb6dd1f84a2b9c338f2046ba06b01
Antonia.icm <antonia.campillay.m@mail.pucv.cl>
2023-09-21 17:50:56 -0300

    empezando de nuevo...

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let canvas;
+let centerX, centerY;
+let staatLichesFont;
+let img;
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function preload() {
+  staatLichesFont = loadFont('Staatliches-Regular.ttf');
+  img = loadImage('softGradient-pastelPink.jpg');
+}
+
+function setup() {
+  canvas= createCanvas(windowWidth, windowHeight, WEBGL);
+  background(99, 111, 175);
+  
+  centerX = windowWidth / 2;
+  centerY = windowHeight / 2;
+    
+}
+
+function draw() {
+
+  //planeta
+  noStroke();
+  rotateY(millis() / 1600);
+  sphere(215);
+  
+  texture(img);
+  sphere(215);
+
+
+}
